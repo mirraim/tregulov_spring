477503270ebdb19218c7e70c606d8eb1f175256d
Mirraim <mirraim@gmail.com>
2021-10-04 23:56:43 +0500

    init & destroy

--- src/main/java/mirraim/tregulov_spring/introduction/Dog.java
+++ src/main/java/mirraim/tregulov_spring/introduction/Dog.java
@@ -5,4 +5,21 @@ public class Dog implements Pet {
     public void say() {
         System.out.println("Bow-Wow");
     }
+
+    /**
+     * Название может быть любым, главное указать его в конфигурационном файле
+     * Возвращаемый тип может быль любой, но значение мы получить не сможем
+     * Параметров в методах init и destroy быть не должно
+     * init-метод вызывается для каждого созданного бина
+     */
+    public void init() {
+        System.out.println("Class Dog: init method");
+    }
+
+    /**
+     * destroy-метод вызывается только если scope=singleton
+     */
+    public void destroy() {
+        System.out.println("Class Dog: destroy method");
+    }
 }
---
+++ src/main/java/mirraim/tregulov_spring/introduction/InitDestroyTest.java
@@ -0,0 +1,15 @@
+package mirraim.tregulov_spring.introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class InitDestroyTest {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+          "appContext.xml"
+
+        );
+        Dog dog = context.getBean("myPet", Dog.class);
+        dog.say();
+        context.close();
+    }
+}
