df7a0a30c49aa190669569f1224e4ff129f5b35f
Mirraim <mirraim@gmail.com>
2021-10-04 20:58:35 +0500

    DI с помощью сеттера

--- src/main/java/mirraim/tregulov_spring/introduction/Person.java
+++ src/main/java/mirraim/tregulov_spring/introduction/Person.java
@@ -7,6 +7,19 @@ public class Person {
         this.pet = pet;
     }
 
+    public Person() {
+    }
+
+    /**
+     * автоматически формируемый бин будет иметь
+     * name = название метода с маленькой буквы, отбросив set, т.е. "pet"
+     * @param pet pet
+     */
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
+        this.pet = pet;
+    }
+
     public void callYourPet() {
         System.out.println("Hello, my pet!");
         pet.say();
