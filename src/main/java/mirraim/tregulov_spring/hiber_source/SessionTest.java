35b9ec31f6b7a2d6a386d0a6511494e591e91521
Mirraim <mirraim@gmail.com>
2021-10-12 18:50:10 +0500

    Получение Java объектов из БД.

--- src/main/java/mirraim/tregulov_spring/hiber_source/SessionTest.java
+++ src/main/java/mirraim/tregulov_spring/hiber_source/SessionTest.java
@@ -5,6 +5,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class SessionTest {
     private SessionFactory factory;
 
@@ -29,10 +31,14 @@ public class SessionTest {
     public static void main(String[] args) {
        SessionTest store = SessionTest.init();
         try {
-            Employee employee = new Employee(
-                    "Mike", "Hornby", "IT", 800
-            );
-           store.save(employee);
+//            Employee employee = new Employee(
+//                    "Helen", "Parker", "Sales", 650
+//            );
+//           store.save(employee);
+//           int id = employee.getId();
+//           System.out.println(store.getById(id));
+            List<Employee> employees = store.getByName("Mike");
+            employees.forEach(System.out::println);
         } finally {
             store.close(); // SessionFactory всегда должна быть закрыта
         }
@@ -40,8 +46,9 @@ public class SessionTest {
 
     /**
      * Метод сохраняет в БД объект Employee
-     * Соединение с БД обеспечивает объект Session. Этот объект заново создается
-     * для каждого подключения к БД
+     * Соединение с БД обеспечивает объект Session.
+     * Session заново создается для каждого подключения к БД
+     * Между началом транзакции и коммитом можно произвести несколько операций
      * @return employee с измененным id
      */
     public Employee save(Employee employee) {
@@ -50,6 +57,52 @@ public class SessionTest {
         session.save(employee);
         session.getTransaction().commit();
         return employee;
+    }
+
+    /**
+     * Метод получает из БД объект Employee
+     * Поиск производится по id, который принимается как параметр
+     * Соединение с БД обеспечивает объект Session.
+     * @return employee или null в случае отсутствия id в базе
+     */
+    public Employee getById(int id) {
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+        Employee employee = session.get(Employee.class, id);
+        session.getTransaction().commit();
+        return employee;
+    }
+
+    /**
+     * Метод получает из БД список всех объектов Employee
+     * Соединение с БД обеспечивает объект Session.
+     * @return список employee или пустой List
+     */
+    public List<Employee> getList() {
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+        // здесь пишется имя класса, а не имя таблицы
+        List<Employee> employees = session.createQuery("from Employee")
+                        .getResultList();
+        session.getTransaction().commit();
+        return employees;
+    }
+
+    /**
+     * Метод получает из БД объект Employee
+     * Поиск производится по id, который принимается как параметр
+     * Соединение с БД обеспечивает объект Session.
+     * @return employee или null в случае отсутствия id в базе
+     */
+    public List<Employee> getByName(String empName) {
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+        // .createQuery может содержать любой запрос на HQL
+        List<Employee> employees = session.createQuery(
+                "from Employee where name='" + empName + "'"
+                ).getResultList();
+        session.getTransaction().commit();
+        return employees;
     }
 
     /**
